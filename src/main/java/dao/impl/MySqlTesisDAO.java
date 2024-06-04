ea0e563c9272b00c954374cab3b50bf5f36d1c1b
MiguelTFD <MiguelTFD@outlook.com>
2024-06-03 23:01:00 -0500

    New Tesis

--- src/main/java/dao/impl/MySqlTesisDAO.java
+++ src/main/java/dao/impl/MySqlTesisDAO.java
@@ -1,7 +1,17 @@
 package dao.impl;
 
 import dao.TesisDAO;
+import entity.Tesis;
+import java.sql.Connection;
 
 public class MySqlTesisDAO implements TesisDAO {
+
+	 //Insert
+	 @Override
+	 public int insertarTesis(Tesis obj) {
+		 Connection cn;
+		 PreparedStatement pt;
+
+	 }
 
 }
